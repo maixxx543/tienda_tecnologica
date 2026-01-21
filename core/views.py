46b2c04ecb709bd373e5a14476eb3989a154a528
maixxx543 <maikeljhonandrimojicalargo@gmail.com>
2026-01-21 17:01:59 -0500

    actualizacion de html,css, imagenes, views y rutas

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'core/index.html')
+def catalogo(request):
+    return render(request, 'core/catalogo.html')
