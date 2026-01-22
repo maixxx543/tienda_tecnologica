d74ef20ce85a808a06fded4ff712173ed43a5da0
maixxx543 <maikeljhonandrimojicalargo@gmail.com>
2026-01-22 11:03:52 -0500

    urls de html, css, img, nuevas views para html

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
-    path('catalogo/',views.catalogo, name='catalogo')
+    path('catalogo/',views.catalogo, name='catalogo'),
+    path('registro/',views.formulario_registro, name='formulario_registro'),
+    path('inicio_sesion/',views.formulario_inicio_sesion, name='formulario_inicio_sesion'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,5 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'core/index.html')
+
 def catalogo(request):
     return render(request, 'core/catalogo.html')
+
+def formulario_registro(request):
+    return render(request, 'core/formulario_registro.html')
+
+def formulario_inicio_sesion(request):
+    return render(request, 'core/formulario_inicio_sesion.html')
